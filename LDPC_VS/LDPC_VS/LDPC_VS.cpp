179bce6c55d42c9bf2d8e02a1c7cac5b64cb6063
grganzaroli <g.rossiganza@gmail.com>
2018-04-06 16:24:56 -0300

    update

---
+++ LDPC_VS/LDPC_VS/LDPC_VS.cpp
@@ -0,0 +1,79 @@
+// LDPC_VS.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "ldpc.h"
+#include "stdio.h"
+
+#pragma warning	(disable: 4996)
+
+#define nldpc 16200
+#define rate 5
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	ldpc teste;
+
+	unsigned char *in_enc = new unsigned char[nldpc];
+	unsigned char *out_enc = new unsigned char[nldpc];
+	unsigned char *out_dec = new unsigned char[nldpc];
+
+	teste.init(nldpc, nldpc*rate/15);
+
+	for(int j = 0; j < (nldpc); j++)
+	{
+		in_enc[j] = j%2;
+	}
+
+	teste.encode(in_enc, out_enc);
+
+	FILE *G = fopen("out_enc.txt", "w");
+	for (unsigned short j = 0; j < (nldpc); j++)
+	{
+		fprintf(G, "%i", out_enc[j]);
+	}
+	fclose(G);
+	printf("OK out_enc\n");
+
+	teste.alocar_hard();
+
+	//inserindo erro
+	//out_enc[0] ^= 1;
+	out_enc[10] ^= 1;
+	//out_enc[500] ^= 1;
+	//out_enc[1000] ^= 1;
+	//out_enc[1200] ^= 1;
+	//out_enc[1800] ^= 1;
+	//out_enc[2300] ^= 1;
+	out_enc[2700] ^= 1;
+	//out_enc[3100] ^= 1;
+	//out_enc[3400] ^= 1;
+	out_enc[3900] ^= 1;
+	//out_enc[4300] ^= 1;
+	out_enc[4700] ^= 1;
+	//out_enc[5000] ^= 1;
+	//out_enc[5200] ^= 1;
+	//out_enc[5399] ^= 1;
+
+	//teste.decode_hard(out_enc, out_dec);
+	teste.decode_hard2(out_enc, out_dec);
+	//teste.decode_hard3(out_enc, out_dec);
+
+	teste.free_hard();
+
+	int e = 0;
+	for(int i = 0; i < nldpc*rate/15; i++)
+	{
+		if(in_enc[i] != out_dec[i])
+		{
+			printf("ERRO POSICAO %i\n", i);
+			e++;
+		}
+	}
+
+	printf("\nTOTAL: %i ERROS\n", e);
+
+	printf("");
+	return 0;
+}
+
---
+++ LDPC_VS/LDPC_VS/ldpc.h
@@ -0,0 +1,50 @@
+﻿#ifndef LDPC_H
+#define LDPC_H
+
+class ldpc
+{
+private:
+	unsigned short n; //quantidade de bits de entrada
+	unsigned short k; //quantidade de bits de saída
+	unsigned short r; //code rate
+	unsigned short max_it; //numero de iterações
+	unsigned short Q_ldpc; //constante que vem da norma
+	unsigned short M1; //constante da norma para LDPC tipo A
+	unsigned short M2; //constante da norma para LDPC tipo A
+	unsigned short Q1; //constante da norma para LDPC tipo A
+	unsigned short Q2; //constante da norma para LDPC tipo A
+	bool tipo; //false = tipo A, true = tipo B
+
+	unsigned short **C; // matriz de check nodes
+	unsigned short *INDX; // indices para calcular a matriz C, numero de 1s por linha
+
+	unsigned short **err; //contagem de 1s e 0s no hard-decision
+	unsigned char *r_aux; //in para eu manipular
+	double *LLR_aux; //in em formato LLR pra eu manipular
+	unsigned char *SIN; //sindrome
+
+	unsigned short *p_erros; //posicao dos erros
+	double** R; //matriz R
+	double** Q; //matriz Q
+
+public:
+	void init(unsigned short N, unsigned short K); //inicializa variavais
+
+	double aTanh(double x); //arco tangente hiperbólico
+
+	void alocar_hard();//alocação de variáveis para hard_decision
+	void alocar_soft();//alocação de variáveis para soft_decision
+
+	void free_hard();//desalocação de variáveis para hard_decision
+	void free_soft();//desalocação de variáveis para soft_decision
+
+	bool decode_hard(const unsigned char *r, unsigned char *u); //decodifica [r] em [u] -> hard-decision - voto majoritário
+	bool decode_hard2(const unsigned char *r, unsigned char *u); //decodifica [r] em [u] -> hard-decision - maior numero de erros, inverte todos
+	bool decode_hard3(const unsigned char *r, unsigned char *u); //decodifica [r] em [u] -> hard-decision - maior numero de erros, inverte 1 por vez
+	bool decode_soft(const float *r, unsigned char *u); //decodifica [r] em [u] -> soft-decision
+
+	bool encode(const unsigned char *u, unsigned char *v); //codifica [u] em [v]
+
+};
+
+#endif
